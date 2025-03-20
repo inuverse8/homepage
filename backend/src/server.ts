5ced840f7d1d0abcc1c76b687d55bedd272074f7
Tatsuki Kodama <namer-namer@outlook.jp>
2025-03-20 09:16:42 +0900

    [update]

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,44 +1,14 @@
-import * as express from "express";
-import * as cors from "cors";
-import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import blogRoutes from "./routes/blogRoutes";
 
 dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-
-interface Blog {
-  id: number;
-  title: string;
-  content: string;
-}
-
-const blogs: Blog[] = [];
-
-app.get("/blogs", (req, res) => {
-  res.json(blogs);
-});
-
-app.post("/blogs", (req, res) => {
-  const newBlog: Blog = { id: Date.now(), ...req.body };
-  blogs.push(newBlog);
-  res.status(201).json(newBlog);
-});
-
-app.get("/blogs/:id", (req, res) => {
-  const blog = blogs.find(b => b.id === Number(req.params.id));
-  blog ? res.json(blog) : res.status(404).json({ error: "Blog not found" });
-});
-
-app.delete("/blogs/:id", (req, res) => {
-  const index = blogs.findIndex(b => b.id === Number(req.params.id));
-  if (index !== -1) {
-    blogs.splice(index, 1);
-    res.status(204).send();
-  } else {
-    res.status(404).json({ error: "Blog not found" });
-  }
-});
+app.use("/blogs", blogRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
